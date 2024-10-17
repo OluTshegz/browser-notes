9dc11a946ea1c4864d6c75a00765f34382b0e604
Olusegun <ayosegun2001@gmail.com>
2024-10-17 14:48:58 +0100

    Fourth commit: Creating custom models and serializing the models from the database to the view

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"] # fields to be serialized
-        extra_kwargs = {"password": {"write_only": True}} # password is write only
+        extra_kwargs = {"password": {"write_only": True}} # password is write only, written at creation, can't be read when viewing `fields`
 
     def create(self, validated_data):
-        print(validated_data)
+        # print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}} # author is read only, can't be written when creating a note
