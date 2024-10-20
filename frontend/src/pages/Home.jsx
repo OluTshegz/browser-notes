7c7ef3502e5b662574dcb305e700722602f9e15d
Olusegun <ayosegun2001@gmail.com>
2024-10-20 16:15:17 +0100

    created and added the note component to the rendered home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import api from "../api";
+import Note from "../components/Note";
 
 function Home () {
     const [notes, setNotes] = useState([]);
@@ -43,6 +44,7 @@ function Home () {
 
             <div>
                 <h2>Notes</h2>
+                { notes.map((note) => < Note note={note} onDelete={ deleteNote } key={ note.id } />) }
             </div>
 
             <h2>Create a Note</h2>
