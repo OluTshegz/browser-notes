dfb6a10efac7080b11f48e9a07b51d1d54ffdbb6
Olusegun <ayosegun2001@gmail.com>
2024-10-20 17:29:09 +0100

    styled the components and added a loading indicator wheel: finishing touches to the frontend

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,13 +3,15 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css";
-
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+
+    const name = method === "login" ? "Login" : "Register";
 
     const handleSubmit = async (e) => {
         setLoading(true);
@@ -31,25 +33,16 @@ function Form({ route, method }) {
         }
     };
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-    };
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
-
-    const name = method === "login" ? "Login" : "Register";
-
     return (
         <form onSubmit={ handleSubmit } className="form-container">
             <h1>{ name }</h1>
             <input className="form-input" type="text" value={username}
-                onChange={ handleUsernameChange } placeholder="Username"
+                onChange={ (e) => setUsername(e.target.value) } placeholder="Username"
             />
             <input className="form-input" type="password" value={password}
-                onChange={ handlePasswordChange } placeholder="Password"
+                onChange={ (e) => setPassword(e.target.value) } placeholder="Password"
             />
+            { loading && <LoadingIndicator /> }
             <button className="form-button" type="submit">{ name }</button>
         </form>
     );
