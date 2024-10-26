82bd997d2b4fa693d769e2e24136774f1f8f173c
Olusegun <ayosegun2001@gmail.com>
2024-10-26 01:18:13 +0100

    Wrap App with AuthContextProvider for context access by child components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,20 @@ import Home from "./pages/Home"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 
+import AuthContextProvider from "./context/AuthContextProvider"
+
 function App() {
   return (
     <>
       <div className="text-4xl">Browser Notes</div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Home /> }></Route>
-          <Route path="/register" element={ <Signup /> }></Route>
-          <Route path="/login" element={ <Login /> }></Route>
-        </Routes>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={ <Home /> }></Route>
+            <Route path="/register" element={ <Signup /> }></Route>
+            <Route path="/login" element={ <Login /> }></Route>
+          </Routes>
+        </AuthContextProvider>
       </BrowserRouter>
     </>
   )
