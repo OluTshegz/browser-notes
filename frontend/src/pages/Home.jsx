b68d24e9187c8d4e13a6b7a4a591692a0cc35f0f
Olusegun <ayosegun2001@gmail.com>
2024-10-28 17:14:17 +0100

    Implemented functionality to add notes using a modal and user authentication

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react"
+import axios from "axios"
+
 import Navbar from "../components/Navbar"
+import NoteModal from "../components/NoteModal"
 
 const Home = () => {
+
+    const [isModalOpen, setModalOpen] = useState(false)
+    const closeModal = () => {
+        setModalOpen(false)
+    }
+    const addNote = async (title, description) => {
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/api/note/add",
+                { title, description }, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`
+                    }
+                }
+            )
+            console.log(response)
+            if (response.data.success) {
+                closeModal()
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <>
-            <div className="text-2xl">Home</div>
-            <Navbar />
+            <div className="bg-gray-100 min-h-screen">
+                Home
+                <Navbar />
+                <button onClick={ () => setModalOpen(true) }
+                    className="fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-2 rounded-full">
+                    +
+                </button>
+                { (isModalOpen) && <NoteModal closeModal={ closeModal }  addNote={ addNote } /> }
+            </div>
         </>
     )
 }
