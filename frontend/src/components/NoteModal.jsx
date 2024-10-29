6b74671dc4bc640b5eccf6526359b624a67bb71b
Olusegun <ayosegun2001@gmail.com>
2024-10-29 15:00:13 +0100

    implemented editing functionality to this Note Modal component card with it rendering the desired layout

--- frontend/src/components/NoteModal.jsx
+++ frontend/src/components/NoteModal.jsx
@@ -1,28 +1,38 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-const NoteModal = ({ closeModal, addNote }) => {
+const NoteModal = ({ closeModal, addNote, currentNote, editNote }) => {
 
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
 
+    useEffect(() => {
+        if (currentNote) {
+            setTitle(currentNote.title)
+            setDescription(currentNote.description)
+        }
+    }, [currentNote])
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        addNote(title, description)
+        if (currentNote) {
+            editNote(currentNote._id, title, description)
+        } else {
+            addNote(title, description)
+        }
     }
 
     return (
         <>
-            <div>NoteModal</div>
             <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center" >
                 <div className="bg-white p-8 rounded" >
-                    <h2 className="text-xl font-bold mb-4" >Add your new note:</h2>
+                    <h2 className="text-xl font-bold mb-4" >{ (currentNote) ? "Edit this current note:" : "Add a new note:" }</h2>
                     <form onSubmit={ handleSubmit }>
                         <input type="text" value={ title } onChange={ (e) => setTitle(e.target.value) }
                             placeholder="Note Title" className="border p-2 w-full mb-4" />
                         <textarea value={ description } onChange={ (e) => setDescription(e.target.value) }
                             placeholder="Note Description" className="border p-2 w-full mb-4" />
                         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-                            Add Note
+                            { (currentNote) ? "Update this note" : "Add this note" }
                         </button>
                     </form>
                     <button className="mt-4 text-red-500" onClick={ closeModal }>Cancel</button>
